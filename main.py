967ddb34bdd19646a343c97528cf3da3f20fa813
Datsteelfn2 <Datsteelfn@gmail.com>
2025-01-04 02:00:37 -0500

    adding and viewing tasks inputs and outputs are functional, but id like the screen to fully disappear when viewing tasks to only show tasks

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import os
 import time
 tasks=[]
-we want to access this list everywhere so we keep it as is
+#we want to access this list everywhere so we keep it as is
 def view_tasks():
     for task in range(len(tasks)):
         print(f"#{task}: {tasks[task]}")
 
 def add_tasks():
     add=input("What would you like to add:> ")
-    add.append(add)
+    tasks.append(add)
 
 while True:
+    os.system("clear")
     choice=int(input("1. Do you want to Add a task\n2. Delete a task,\n3. View a task,\n4. Mark a task as done or\n5. Exit the program\n:> "))
-
+    if choice==1:
+        add_tasks()
     elif choice==3:
-        view_tasks
+        os.system("clear")
+        view_tasks()
+        time.sleep(2)
     elif choice==5:
         break
+    os.system("clear")
+    
 print("Youve exited the program")
