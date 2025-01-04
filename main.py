a80d4755c2510fd3de9092f1eb798c12598d06ad
Datsteelfn2 <Datsteelfn@gmail.com>
2025-01-04 03:17:16 -0500

    made a feature that allows one to mark a task as done, as well as human errors checkers if a human were to give a wrong input

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import os
 import time
-tasks=[]
-#we want to access this list everywhere so we keep it as is
-def view_tasks():
-    
-    os.system("clear")
-    for task in range(len(tasks)):
-        print(f"{task+1}: {tasks[task]}")
-    time.sleep(3)
+tasks=[]#we want to access this list everywhere so we keep it outside any function to make it global
+done_tasks=[]
 def add_tasks():
     os.system("clear")
     add=input("What would you like to add:> ")
     tasks.append(add)
+
+
 def delete_task():
+    os.system("clear")
     print("You may only delete \033[31mone\033[0m task at a time")
     name_delete=input("NAME the task you wanat to delete:> ")
     if name_delete in tasks:
@@ -21,22 +18,48 @@ def delete_task():
         time.sleep(1.5)
     else:
         print(f"{name_delete} is not in your tasks list, press 3 on your menu to see your tasks names")
+        time.sleep(3)
 
-while True:
+
+def view_tasks():
+    os.system("clear")
+    for task in range(len(tasks)):
+        print(f"{task+1}: {tasks[task]}")
+    time.sleep(3)
+
+def task_done():
+    #lets print a list using a different method because why not
+    counter=1
+    os.system("clear")
     
-    choice=int(input("1. Do you want to Add a task\n2. Delete a task,\n3. View a task,\n4. Mark a task as done or\n5.Exit the program\n:> "))
+    whats_done=input("NAME the task you want to mark as done, can only check \033[31mONE TASK\033[0m")
+    print("DONE TASKS")
+    if whats_done in tasks:
+        done_tasks.append(whats_done)
+        tasks.remove(whats_done)
+        for done in done_tasks:
+            print(f"{counter}: {done}")
+    else:
+        print("\033[31m That is not a task on your list\033[0m")
+    time.sleep(3)
+while True:
+    print("MENU")
+    choice=int(input("1. Do you want to Add a task\n2. Delete a task,\n3. View tasks,\n4. Mark a task as done\n5. Exit the program\n:> "))
     if choice==1:
         
         add_tasks()
     elif choice==2:
         delete_task()
     elif choice==3:
-        
         view_tasks()
-        
+    elif choice==4:
+        task_done()
     elif choice==5:
         break
     
+    else:
+        print("not a valid command, type one of the following: 1, 2, 3, 4, 5, or 6 (to exit the program) with no spaces")
+        time.sleep(3)
     os.system('clear')
     
 print("Youve exited the program")
