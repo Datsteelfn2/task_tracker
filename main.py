abb38351300b8b084dc5e55cc13b77527f6d3add
Datsteelfn2 <Datsteelfn@gmail.com>
2025-01-04 01:47:32 -0500

    added a add function as well as a way to navigate through task 1 and 2

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import os
 import time
-
+tasks=[]
+we want to access this list everywhere so we keep it as is
 def view_tasks():
-    pass
+    for task in range(len(tasks)):
+        print(f"#{task}: {tasks[task]}")
 
+def add_tasks():
+    add=input("What would you like to add:> ")
+    add.append(add)
 
 while True:
-    choice=int(input("1. Do you want to Add a task\n2. Delete a task,\n3. View a task,\n4. Mark a task as done or\n5. Exit the program"))
-    print(":> ")
-    if choice==5:
+    choice=int(input("1. Do you want to Add a task\n2. Delete a task,\n3. View a task,\n4. Mark a task as done or\n5. Exit the program\n:> "))
+
+    elif choice==3:
+        view_tasks
+    elif choice==5:
         break
 print("Youve exited the program")
