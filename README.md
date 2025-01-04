882a349c20d3633ba1ecfedd085b567e32897192
Datsteelfn2 <Datsteelfn@gmail.com>
2025-01-04 01:42:01 -0500

    basic exit to the program using user input

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # task_tracker
+This program is used to set and track ur tasks
---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+def view_tasks():
+    pass
+
+
+while True:
+    choice=int(input("1. Do you want to Add a task\n2. Delete a task,\n3. View a task,\n4. Mark a task as done or\n5. Exit the program"))
+    print(":> ")
+    if choice==5:
+        break
+print("Youve exited the program")
